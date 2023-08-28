4895ef571f543d5f231846d13a0a9d49f3845eec
earmer <65227354+earmer@users.noreply.github.com>
2023-08-28 22:33:44 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2023 LTL Team. All rights reserved.
+# Author: Earmer Carey
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from Wafflefront import Ui_MainWindow
+class MyWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MyWindow, self).__init__(parent)
+        self.setupUi(self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    myWin = MyWindow()
+    myWin.show()
+    sys.exit(app.exec_())
