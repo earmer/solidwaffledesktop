f68deabb12837c41f746d41e2526262da841df5d
earmer <65227354+earmer@users.noreply.github.com>
2023-08-29 13:17:39 +0800

    Add clear, able / disable features.

--- main.py
+++ main.py
@@ -2,15 +2,18 @@
 # Author: Earmer Carey
 
 import sys
+
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from Wafflefront import Ui_MainWindow
-class MyWindow(QMainWindow, Ui_MainWindow):
+from Wafflefront import Ui_Solidwaffle
+class MyWindow(QMainWindow, Ui_Solidwaffle):
     def __init__(self, parent=None):
         super(MyWindow, self).__init__(parent)
         self.setupUi(self)
 
 
 if __name__ == '__main__':
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
     app = QApplication(sys.argv)
     myWin = MyWindow()
     myWin.show()
